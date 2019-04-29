ed411a5259379707b27acefc6408a8a436d5a598
Austin Beaufort <tinyhatcoding@gmail.com>
2019-04-28 22:30:05 -0400

    Problem 1 Completed with JavaScript

--- problem1.js
+++ problem1.js
@@ -2,4 +2,20 @@
 
 // Problem 1.
 
-console.log('hi');
+function addMultiples(num) {
+    let arrayToAdd = findMultiples(num);
+    let total = arrayToAdd.reduce((a, b) => a + b);
+    return total;
+}
+
+function findMultiples() {
+    let multiplesArray = [];
+    for (let i = 1; i < 1000; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            multiplesArray.push(i);
+        }
+    }
+    return multiplesArray;
+}
+
+console.log(addMultiples(1000));
