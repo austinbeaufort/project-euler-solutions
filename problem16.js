2a785f7a468cc9ff846e5e4e1ae9359d056a1bee
Austin Beaufort <tinyhatcoding@gmail.com>
2019-07-05 11:00:34 -0400

    problem 13 solved using BigInt

--- problem16.js
+++ problem16.js
@@ -5,7 +5,7 @@ let largeNumber = String(BigInt(2**1000));
 function addDigitsOfLargeNumber(largeNumber) {
     let numArray = largeNumber.split('');
     let answer = h.add(numArray);
-    console.log(answer);
+    return answer;
 }
 
-addDigitsOfLargeNumber(largeNumber);
+console.log(addDigitsOfLargeNumber(largeNumber));
